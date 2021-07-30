cc507bb4f746461d07189abdf53078bd8814edc8
Rafael de Oliveira Romano <rafa-kun@hotmail.com>
2021-07-30 18:53:00 -0300

    :sparkles: Adicionando requisito
    Foi feito o path do primeiro requisito

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import { Switch,BrowserRouter, Route} from 'react-router-dom';
 import './App.css';
 import pokemons from './data';
 import Pokedex from './Pokedex';
 
 function App() {
   return (
+    <BrowserRouter>
+    <Switch>
+    <Route path="/" render={(props)=> <Pokedex pokemons={pokemons}/>}/>
     <div className="App">
       <h1> Pokedex </h1>
       <h2>Rafael Romano</h2>
-      <Pokedex pokemons={pokemons} />
     </div>
+    </Switch>
+    </BrowserRouter>
   );
 }
 
